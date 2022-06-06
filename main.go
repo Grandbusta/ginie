5e1de04ce085b81ad61820e6bce1af94b455a861
Grandbusta <bustajay30@gmail.com>
2022-06-07 00:49:13 +0100

    update

--- main.go
+++ main.go
@@ -14,12 +14,24 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func webhookPOST(w http.ResponseWriter, r *http.Request) {
-	var response structs.Response
-	b, _ := ioutil.ReadAll(r.Body)
+func selectService(phone_number_id, from, profile_name, msg_body string) {
 	services := map[string]string{
 		"crypto": "crypto",
 	}
+	msg_split := strings.Split(strings.ToLower(msg_body), " ")
+	if val, ok := services[msg_split[0]]; ok {
+		if val == "crypto" {
+			status := lib.GetCryptoPrice(phone_number_id, from, profile_name, msg_split)
+			fmt.Println(status)
+		}
+	} else {
+		fmt.Println("Command not found")
+	}
+}
+
+func webhookPOST(w http.ResponseWriter, r *http.Request) {
+	var response structs.Response
+	b, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal(b, &response)
 	if len(response.Object) > 0 {
 		if len(response.Entry) > 0 && len(response.Entry[0].Changes) > 0 && len(response.Entry[0].Changes[0].Value.Messages) > 0 {
@@ -28,15 +40,7 @@ func webhookPOST(w http.ResponseWriter, r *http.Request) {
 			msg_body := response.Entry[0].Changes[0].Value.Messages[0].Text.Body
 			profile_name := response.Entry[0].Changes[0].Value.Contacts[0].Profile.Name
 			fmt.Println(phone_number_id, from, profile_name, msg_body)
-			msg_split := strings.Split(strings.ToLower(msg_body), " ")
-			if val, ok := services[msg_split[0]]; ok {
-				if val == "crypto" {
-					status := lib.GetCryptoPrice(phone_number_id, from, profile_name, msg_split)
-					fmt.Println(status)
-				}
-			} else {
-				fmt.Println("Command not found")
-			}
+			selectService(phone_number_id, from, profile_name, msg_body)
 		}
 	}
 }
